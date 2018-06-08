914989ba94884695970c550e7dbc8d57dae84201
Dopa99 <tychus77@naver.com>
2018-06-08 22:27:09 +0900

    CryptoCurrency 클래스 기능 구현
    JsonArray를 LinkedList로 대체, etc...

--- BlackCow/src/upbit/CryptoCurrency.java
+++ BlackCow/src/upbit/CryptoCurrency.java
@@ -1,36 +1,147 @@
 package upbit;
 
+import java.io.Serializable;
+import java.util.LinkedList;
+
 import org.json.simple.JSONObject;
 
 import upbit.JsonManager.JsonKey;
+import upbit.Request.TermType;
 
-public class CryptoCurrency
+public class CryptoCurrency implements Serializable
 {	
-	private JSONObject jsonObject;
+	private LinkedList<JSONObject> objectList;
 	
-	public CryptoCurrency(JSONObject jsonObject)
+	private String name;
+	private boolean byMinute = false;
+
+	private Market market;
+	private CoinSymbol coinSymbol;
+	private TermType termType;
+	
+	public CryptoCurrency(LinkedList<JSONObject> list, String name, Market market, CoinSymbol coinSymbol, TermType termType)
 	{
-		this.setJsonObject(jsonObject);
+		this.setObjecList(list);
+		this.setName(name);
+		this.setMarket(market);
+		this.setCoinSymbol(coinSymbol);
+		this.setTermType(termType);
 	}
 
-	public JSONObject getJsonObject()
+	public void addData(LinkedList<JSONObject> list)
 	{
-		return jsonObject;
-	}
+		int index = 0;
+		long base, input;
+		boolean success = true;
+		
+		for (JSONObject inputObject : list)
+		{
+			input = Long.parseLong(JsonManager.getData(inputObject, JsonKey.timestamp));
+			
+			if (success == true)
+			{
+				success = false;
 
-	public void setJsonObject(JSONObject jsonObject)
-	{
-		this.jsonObject = jsonObject;
+				for (; index < objectList.size(); index++)
+				{
+					base = Long.parseLong(JsonManager.getData(objectList.get(index), JsonKey.timestamp));
+					
+					if (input < base)
+					{
+						objectList.add(index, inputObject);
+						success = true;
+						break;
+					}
+					
+					if (input == base)
+					{
+						success = true;
+						break;
+					}
+				}
+			}
+			
+			if (success == false)
+				objectList.addLast(inputObject);
+		}
 	}
 	
 	public String getData(JsonKey jsonKey)
 	{
-		return JsonManager.getData(this.jsonObject, jsonKey);
+		return getData(jsonKey, 0);
 	}
 	
-	// 테스트용 함수
+	public String getData(JsonKey jsonKey, int index)
+	{
+		return JsonManager.getData(this, jsonKey, index);
+	}
+	
+	public JSONObject getJsonObject(int index)
+	{
+		return objectList.get(index);
+	}
+	
+	public int getSize()
+	{
+		return objectList.size();
+	}
+	
+	public LinkedList<JSONObject> getObjectList()
+	{
+		return objectList;
+	}
+	
+	public void setObjecList(LinkedList<JSONObject> list)
+	{
+		this.objectList = list;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public boolean isByMinute()
+	{
+		return byMinute;
+	}
+
+	public Market getMarket()
+	{
+		return market;
+	}
+
+	public void setMarket(Market market)
+	{
+		this.market = market;
+	}
 	public CoinSymbol getCoinSymbol()
 	{
-		return CoinSymbol.BTC;
+		return coinSymbol;
+	}
+
+	public void setCoinSymbol(CoinSymbol coinSymbol)
+	{
+		this.coinSymbol = coinSymbol;
+	}
+
+	public TermType getTermType()
+	{
+		return termType;
+	}
+	
+	public void setTermType(TermType termType)
+	{
+		this.termType = termType;
+		
+		if (termType == TermType.minutes)
+			byMinute = true;
+		else
+			byMinute = false;
 	}
 }
