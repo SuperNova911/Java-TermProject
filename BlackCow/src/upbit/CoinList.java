f6ad227e8993d883071bf23082e5bd073c5a8a54
Dopa99 <tychus77@naver.com>
2018-06-12 21:43:20 +0900

    띄어쓰기

--- BlackCow/src/upbit/CoinList.java
+++ BlackCow/src/upbit/CoinList.java
@@ -18,14 +18,14 @@ public class CoinList
 		SC, ETH, SNT, QTUM, BCC, NEO, SBD, ETC, ARDR, GRS,
 		XMR, STEEM, GNT, ICX, LSK, XEM, POWR, MER, EMC2,
 		STRAT, OMG, LTC, TIX, BTG, PIVX, MCO, WAVES, STORJ,
-		DASH, KMD, VTC, MTL, REP, ARK, ZEC,XVG,PART,SRN,DGB,
-		NXT,POLY,RDD,DCR,TUSD,ADDR,ZRX,ZEN,DOGE,OCN,PAY,GAME,
-		NXS,WAX,VEE,EDG,XZC,RLC,SYS,TUBE,BAT,BLOCK,FCT,MONA,
-		VIB,SALT,MANA,ADX,DCT,BURST,GUP,RVR,LBC,CLOAK,GBYTE,MUE,
-		VIA,CVC,SLS,ENG,DMT,LRC,DNT,UKG,IOP,BAY,ADT,LUN,OK,HMQ,
-		XEL,XDN,UBQ,NGC,SHIFT,ANT,BITB,UP,NAV,CFI,RCN,GNO,FTC,BLK,
-		DYN,BCPT,ION,RADS,VRC,NBT,AMP,NMR,WINGS,PTOY,BNT,SYNX,KORE,
-		EXP,AID,BLT,EXCL,BSD,SWT,TX,QRL,SPHR,PRO,MEME,SIB,UNB
+		DASH, KMD, VTC, MTL, REP, ARK, ZEC, XVG, PART, SRN, DGB,
+		NXT, POLY, RDD, DCR, TUSD, ADDR, ZRX, ZEN, DOGE, OCN, PAY, GAME,
+		NXS, WAX, VEE, EDG, XZC, RLC, SYS, TUBE, BAT, BLOCK, FCT, MONA,
+		VIB, SALT, MANA, ADX, DCT, BURST, GUP, RVR, LBC, CLOAK, GBYTE, MUE,
+		VIA, CVC, SLS, ENG, DMT, LRC, DNT, UKG, IOP, BAY, ADT, LUN, OK, HMQ,
+		XEL, XDN, UBQ, NGC, SHIFT, ANT, BITB, UP, NAV, CFI, RCN, GNO, FTC, BLK,
+		DYN, BCPT, ION, RADS, VRC, NBT, AMP, NMR, WINGS, PTOY, BNT, SYNX, KORE,
+		EXP, AID, BLT, EXCL, BSD, SWT, TX, QRL, SPHR, PRO, MEME, SIB, UNB
 	
 	}
 	
@@ -35,15 +35,15 @@ public class CoinList
 		시아코인, 이더리움, 스테이터스네트워크토큰, 퀀텀, 비트코인캐시, 네오, 스팀달러, 이더리움클래식, 아더, 그로스톨코인,
 		모네로, 스팀, 골렘, 아이콘, 리스크, 뉴이코노미무브먼트, 파워렛저, 머큐리, 아인스타이늄,
 		스트라티스, 오미세고, 라이트코인, 블록틱스, 비트코인골드, 피벡스, 모나코, 웨이브, 스토리지,
-		대시, 코모도, 버트코인, 메탈, 어거, 아크, 지캐시 ,버지,파티클,시린토큰,디지바이트,엔엑스티,폴리매쓰,레드코인,디크레드,
-		트루USD, 제로엑스,젠캐시,도지코인,오디세이,텐엑스페이토큰,게임크레딧,넥서스,왁스,블록브이,엣지리스,지코인,아이젝,
-		시스코인,비트튜브,베이직어텐션토큰,블록넷,팩텀,모나코인,바이버레이트,솔트,디센트럴랜드,애드엑스,
-		디센트,버스트코인,구피,레볼루션VR,엘비알와이크레딧,클록코인,바이트볼,모네터리유닛,비아코인,
-		시빅,살루스,이니그마,디마켓,루프링,디스트릭0x,유니코인골드,인터넷오브피플,비트베어,애드코인,
-		루너,오케이캐시,엘라스틱,디지털노트,유빜,나가코인,쉬프트,아라곤,비트빈,업토큰,나브코인,
-		코파운드잇,리피오크레딧네트워크,노시스,페더코인,블랙코인,다이내믹,블록메이슨,아이온,라디움,베리코인,누비츠,에이엠피,
-		뉴메레르,윙스다오,페이션토리,뱅코르,신디케이트,코어코인,익스펜스,에이드코인,블룸,익스클루시브코인,비트센드,스웜시티토큰,
-		트랜스퍼코인,퀀텀리지스턴트렛저,스피어,프로피,메메틱,시베리안체르보츠네츠,언브레이커블코인
+		대시, 코모도, 버트코인, 메탈, 어거, 아크, 지캐시, 버지, 파티클, 시린토큰, 디지바이트, 엔엑스티, 폴리매쓰, 레드코인, 디크레드,
+		트루USD, 제로엑스, 젠캐시, 도지코인, 오디세이, 텐엑스페이토큰, 게임크레딧, 넥서스, 왁스, 블록브이, 엣지리스, 지코인, 아이젝,
+		시스코인, 비트튜브, 베이직어텐션토큰, 블록넷, 팩텀, 모나코인, 바이버레이트, 솔트, 디센트럴랜드, 애드엑스,
+		디센트, 버스트코인, 구피, 레볼루션VR, 엘비알와이크레딧, 클록코인, 바이트볼, 모네터리유닛, 비아코인,
+		시빅, 살루스, 이니그마, 디마켓, 루프링, 디스트릭0x, 유니코인골드, 인터넷오브피플, 비트베어, 애드코인,
+		루너, 오케이캐시, 엘라스틱, 디지털노트, 유빜, 나가코인, 쉬프트, 아라곤, 비트빈, 업토큰, 나브코인,
+		코파운드잇, 리피오크레딧네트워크, 노시스, 페더코인, 블랙코인, 다이내믹, 블록메이슨, 아이온, 라디움, 베리코인, 누비츠, 에이엠피,
+		뉴메레르, 윙스다오, 페이션토리, 뱅코르, 신디케이트, 코어코인, 익스펜스, 에이드코인, 블룸, 익스클루시브코인, 비트센드, 스웜시티토큰,
+		트랜스퍼코인, 퀀텀리지스턴트렛저, 스피어, 프로피, 메메틱, 시베리안체르보츠네츠, 언브레이커블코인
 		
 	}
 	
