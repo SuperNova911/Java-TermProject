6182382778dae2466e13b7e67785db98e6a6f290
Dopa99 <tychus77@naver.com>
2018-06-16 02:25:10 +0900

    주문 정보 클래스

---
+++ BlackCow/src/upbit/Order.java
@@ -0,0 +1,152 @@
+package upbit;
+
+import upbit.CoinList.CoinSymbol;
+import upbit.CoinList.Market;
+
+public class Order
+{
+	private Market market;
+	private CoinSymbol coinSymbol;
+	private int id;
+	private double date;
+	private double tradePrice;
+	private double quantity;
+	private double quantity_Conclusion;
+	private double totalPrice;
+	private boolean buy;
+	private boolean conclusion;
+
+	/**
+	 * @param market 거래마켓
+	 * @param coinSymbol 코인심볼
+	 * @param id 거래ID
+	 * @param date 주문시간
+	 * @param tradePrice 주문가격
+	 * @param quantity 주문수량
+	 * @param buy 거래종류
+	 */
+	public Order(Market market, CoinSymbol coinSymbol, int id, double date, double tradePrice, double quantity, boolean buy)
+	{
+		this.market = market;
+		this.coinSymbol = coinSymbol;
+		this.id = id;
+		this.date = date;
+		this.tradePrice = tradePrice;
+		this.quantity = quantity;
+		this.buy = buy;
+		
+		this.quantity_Conclusion = 0;
+		this.totalPrice = quantity * tradePrice;
+		this.conclusion = false;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Order [market=" + market + ", coinSymbol=" + coinSymbol + ", id=" + id + ", date=" + date
+				+ ", tradePrice=" + tradePrice + ", quantity=" + quantity + ", quantity_Conclusion="
+				+ quantity_Conclusion + ", totalPrice=" + totalPrice + ", buy=" + buy + ", conclusion=" + conclusion
+				+ "]";
+	}
+
+	public Market getMarket()
+	{
+		return market;
+	}
+
+	public void setMarket(Market market)
+	{
+		this.market = market;
+	}
+
+	public CoinSymbol getCoinSymbol()
+	{
+		return coinSymbol;
+	}
+
+	public void setCoinSymbol(CoinSymbol coinSymbol)
+	{
+		this.coinSymbol = coinSymbol;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public double getDate()
+	{
+		return date;
+	}
+
+	public void setDate(double date)
+	{
+		this.date = date;
+	}
+
+	public double getTradePrice()
+	{
+		return tradePrice;
+	}
+
+	public void setTradePrice(double tradePrice)
+	{
+		this.tradePrice = tradePrice;
+	}
+
+	public double getQuantity()
+	{
+		return quantity;
+	}
+
+	public void setQuantity(double quantity)
+	{
+		this.quantity = quantity;
+	}
+
+	public double getQuantity_Conclusion()
+	{
+		return quantity_Conclusion;
+	}
+
+	public void setQuantity_Conclusion(double quantity_Conclusion)
+	{
+		this.quantity_Conclusion = quantity_Conclusion;
+	}
+
+	public double getTotalPrice()
+	{
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice)
+	{
+		this.totalPrice = totalPrice;
+	}
+
+	public boolean isBuy()
+	{
+		return buy;
+	}
+
+	public void setBuy(boolean buy)
+	{
+		this.buy = buy;
+	}
+
+	public boolean isConclusion()
+	{
+		return conclusion;
+	}
+
+	public void setConclusion(boolean conclusion)
+	{
+		this.conclusion = conclusion;
+	}
+
+}
---
+++ BlackCow/src/upbit/TradeHistory.java
@@ -0,0 +1,140 @@
+package upbit;
+
+import upbit.CoinList.CoinSymbol;
+import upbit.CoinList.Market;
+
+public class TradeHistory
+{
+	public enum TradeType
+	{
+		Buy, Sell, Withdraw, Deposit
+	}
+
+	private double date;
+	private Market market;
+	private CoinSymbol coinSymbol;
+	private TradeType tradeType;
+	private double quantity;
+	private double tradePrice;
+	private double orderPrice;
+	private double totalPrice;
+	private double fee;
+
+	/**
+	 * @param date 주문시간
+	 * @param market 거래마켓
+	 * @param coinSymbol 코인심볼
+	 * @param tradeType 거래종류
+	 * @param quantity 거래수량
+	 * @param tradePrice 거래단가
+	 * @param orderPrice 거래금액
+	 * @param totalPrice 정산금액(수수료반영)
+	 * @param fee 수수료
+	 */
+	public TradeHistory(double date, Market market, CoinSymbol coinSymbol, TradeType tradeType, double quantity,
+			double tradePrice, double orderPrice, double totalPrice, double fee)
+	{
+		this.date = date;
+		this.market = market;
+		this.coinSymbol = coinSymbol;
+		this.tradeType = tradeType;
+		this.quantity = quantity;
+		this.tradePrice = tradePrice;
+		this.orderPrice = orderPrice;
+		this.totalPrice = totalPrice;
+		this.fee = fee;
+	}
+	
+	
+	
+	public double getDate()
+	{
+		return date;
+	}
+
+	public void setDate(double date)
+	{
+		this.date = date;
+	}
+
+	public Market getMarket()
+	{
+		return market;
+	}
+
+	public void setMarket(Market market)
+	{
+		this.market = market;
+	}
+
+	public CoinSymbol getCoinSymbol()
+	{
+		return coinSymbol;
+	}
+
+	public void setCoinSymbol(CoinSymbol coinSymbol)
+	{
+		this.coinSymbol = coinSymbol;
+	}
+
+	public TradeType getTradeType()
+	{
+		return tradeType;
+	}
+
+	public void setTradeType(TradeType tradeType)
+	{
+		this.tradeType = tradeType;
+	}
+
+	public double getQuantity()
+	{
+		return quantity;
+	}
+
+	public void setQuantity(double quantity)
+	{
+		this.quantity = quantity;
+	}
+
+	public double getTradePrice()
+	{
+		return tradePrice;
+	}
+
+	public void setTradePrice(double tradePrice)
+	{
+		this.tradePrice = tradePrice;
+	}
+
+	public double getOrderPrice()
+	{
+		return orderPrice;
+	}
+
+	public void setOrderPrice(double orderPrice)
+	{
+		this.orderPrice = orderPrice;
+	}
+
+	public double getTotalPrice()
+	{
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice)
+	{
+		this.totalPrice = totalPrice;
+	}
+
+	public double getFee()
+	{
+		return fee;
+	}
+
+	public void setFee(double fee)
+	{
+		this.fee = fee;
+	}
+
+}
