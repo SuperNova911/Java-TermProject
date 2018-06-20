2b3f97b42901b1e367749cc7c3c708d72c4446b5
Dopa99 <tychus77@naver.com>
2018-06-21 03:57:26 +0900

    OrderBook.updateOrderBook() 함수 추가

--- BlackCow/src/upbit/OrderBook.java
+++ BlackCow/src/upbit/OrderBook.java
@@ -12,6 +12,9 @@ public class OrderBook
 	private Market market;
 	private CoinSymbol coinSymbol;
 	
+	private int showIndex;
+	private int tradeIndex;
+	
 	public enum OrderData
 	{
 		price, quantity, percentage
@@ -23,15 +26,15 @@ public class OrderBook
 		setCoinSymbol(coinSymbol);
 		
 		orderBookElementList = new ArrayList<OrderBookElement>();
+		showIndex = 0;
 	}
 	
-	public void addOrderBookElement(String price, String quantity, String percentage, boolean buy)
-	{
-		orderBookElementList.add(new OrderBookElement(price, quantity, percentage, buy));
-//		System.out.println("Add order: " + orderList.size() + ". " + price + "\t" + quantity + "\t" + percentage);
-	}
+//	public void addOrderBookElement(String price, String quantity, String percentage, boolean buy)
+//	{
+//		orderBookElementList.add(new OrderBookElement(price, quantity, percentage, buy));
+//	}
 	
-	public OrderBookElement getOrder(int index)
+	public OrderBookElement getOrderBookElement(int index)
 	{
 		try
 		{
@@ -42,6 +45,102 @@ public class OrderBook
 			System.out.println("Failed to getOrder: " + index);
 			return new OrderBookElement("0", "0", "0", true);
 		}
+	}
+	
+	public void updateOrderBook(OrderBook input)
+	{
+//		int startIndex;
+//		
+//		ArrayList<OrderBookElement> inputList = input.getOrderBookElementList();
+//
+//		int searchIndex;
+//		
+//		for (searchIndex = 0; searchIndex < 10; searchIndex++)
+//		{
+//			for (int i = 0; i < showIndex + 10; i ++)
+//			{
+//				if (getOrderBookElement(i).getPrice() == input.getOrderBookElement(searchIndex).getPrice())
+//				{
+//					startIndex = i;
+//					
+//				}
+//			}
+//		}
+//		
+//		// 가격 내려감, 위의 리스트는 그냥 둠
+//		orderBookElementList.addAll(inputList);
+//		startIndex = showIndex + 10;
+		
+		ArrayList<OrderBookElement> baseList = getOrderBookElementList();
+		ArrayList<OrderBookElement> inputList = input.getOrderBookElementList();
+		ArrayList<OrderBookElement> newList = new ArrayList<OrderBookElement>();
+		
+		int baseIndex, inputIndex, newIndex;
+		baseIndex = inputIndex = newIndex = 0;
+		
+		double basePrice, inputPrice;
+		
+		while (baseIndex < baseList.size() && inputIndex < inputList.size())
+		{
+			if (!(baseIndex < baseList.size()))
+			{
+				for (; inputIndex < inputList.size(); inputIndex++, newIndex++)
+				{
+					if (inputList.get(inputIndex).isNowTrading())
+						tradeIndex = newIndex;
+					
+					newList.add(inputList.get(inputIndex));
+				}
+				
+				break;
+			}
+			else if (!(inputIndex < inputList.size()))
+			{
+				for (; baseIndex < baseList.size(); baseIndex++)
+				{
+					baseList.get(baseIndex).setNowTrading(false);
+					newList.add(baseList.get(baseIndex));
+				}
+				
+				break;
+			}
+			
+			basePrice = baseList.get(baseIndex).getPrice();
+			inputPrice = inputList.get(inputIndex).getPrice();
+						
+			if (basePrice > inputPrice)
+			{
+				baseList.get(baseIndex).setNowTrading(false);
+				newList.add(baseList.get(baseIndex));
+				
+				baseIndex++;
+			}
+			else if (basePrice < inputPrice)
+			{
+				newList.add(inputList.get(inputIndex));
+				
+				if (inputList.get(inputIndex).isNowTrading())
+					tradeIndex = newIndex;
+				
+				inputIndex++;
+			}
+			else
+			{
+				baseList.get(baseIndex).setNowTrading(false);
+				newList.add(baseList.get(baseIndex));
+				newList.get(newIndex).updateElement(inputList.get(inputIndex));
+				
+				if (inputList.get(inputIndex).isNowTrading())
+					tradeIndex = newIndex;
+
+				baseIndex++;
+				inputIndex++;
+			}
+			
+			newIndex++;
+		}
+
+		
 	}
 	
 	public ArrayList<OrderBookElement> getOrderBookElementList()
@@ -67,5 +166,25 @@ public class OrderBook
 	public void setCoinSymbol(CoinSymbol coinSymbol)
 	{
 		this.coinSymbol = coinSymbol;
+	}
+
+	public int getShowIndex()
+	{
+		return showIndex;
+	}
+
+	public void setShowIndex(int showIndex)
+	{
+		this.showIndex = showIndex;
+	}
+
+	public int getTradeIndex()
+	{
+		return tradeIndex;
+	}
+
+	public void setTradeIndex(int tradeIndex)
+	{
+		this.tradeIndex = tradeIndex;
 	}
 }
