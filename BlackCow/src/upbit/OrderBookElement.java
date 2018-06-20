f86d636232683083c69757dab409cd15f3a742fc
Dopa99 <tychus77@naver.com>
2018-06-21 03:57:11 +0900

    OrderBookElement.updateElement() 함수 추가
    boolean nowTrading 변수 추가

--- BlackCow/src/upbit/OrderBookElement.java
+++ BlackCow/src/upbit/OrderBookElement.java
@@ -1,11 +1,16 @@
 package upbit;
 
+import java.util.LinkedList;
+
 public class OrderBookElement
 {
+	private LinkedList<Double> quantityList;
+	
 	private double price;
 	private double quantity;
 	private double percentage;
 	private boolean buy;
+	private boolean nowTrading;
 	
 	public OrderBookElement(String price, String quantity, String percentage, boolean buy)
 	{
@@ -13,14 +18,56 @@ public class OrderBookElement
 		this.setQuantity(toDouble(quantity));
 		this.setPercentage(toDouble(percentage));
 		this.setBuy(buy);
+		this.nowTrading = false;
+
+		quantityList = new LinkedList<Double>();
+		quantityList.add(this.quantity);
+	}
+	
+	public void updateElement(OrderBookElement element)
+	{
+		if (this.quantity > element.getQuantity())
+		{
+			double sell = this.quantity - element.getQuantity();
+			double listData;
+			
+			for (int index = 0; sell > 0; index++)
+			{
+				listData = quantityList.get(index);
+				
+				if (listData >= sell)
+				{
+					listData -= sell;
+					break;
+				}
+				else
+				{
+					sell -= quantityList.get(index);
+					quantityList.set(index, (double) 0);
+				}
+			}
+			
+			for (Double q : quantityList)
+			{
+				if (q == 0)
+					quantityList.remove(q);
+			}
+		}
+		else if (this.quantity < element.getQuantity())
+		{
+			double buy = this.quantity - element.getQuantity();
+			
+			quantityList.add(buy);
+		}
+		
+		setNowTrading(element.isNowTrading());
 	}
 	
 	private double toDouble(String string)
 	{
 		double result = 0;
 
-		string = string.replaceAll(",", "");
-		string = string.replaceAll("%", "");
+		string = string.replaceAll(",", "").replaceAll("%", "");
 		
 		try
 		{
@@ -73,5 +120,15 @@ public class OrderBookElement
 	public void setBuy(boolean buy)
 	{
 		this.buy = buy;
+	}
+
+	public boolean isNowTrading()
+	{
+		return nowTrading;
+	}
+
+	public void setNowTrading(boolean nowTrading)
+	{
+		this.nowTrading = nowTrading;
 	}
 }
